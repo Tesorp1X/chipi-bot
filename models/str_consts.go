784fddc7ae9db11725d4228d1c10ec24c446aabc
Tesorp1X <rakteam2014@gmail.com>
2025-05-18 06:23:30 +0700

    New consts

--- models/str_consts.go
+++ models/str_consts.go
@@ -3,9 +3,18 @@ package models
 const (
 	CHECK_OWNER = "check_owner"
 	CHECK_NAME  = "check_name"
-	OWNER_LIZ   = "liz"
-	OWNER_PAU   = "pau"
-	ITEM_NAME   = "item_name"
+
+	OWNER_LIZ  = "liz"
+	OWNER_PAU  = "pau"
+	OWNER_BOTH = "both"
+
+	ITEM_NAME  = "item_name"
+	ITEM_PRICE = "item_price"
+	ITEM_OWNER = "item_owner"
+
+	HAS_MORE_ITEMS       = "has_more_items"
+	HAS_MORE_ITEMS_TRUE  = "true"
+	HAS_MORE_ITEMS_FALSE = "false"
 )
 
 // error msgs
@@ -13,5 +22,6 @@ const (
 	ErrorSometingWentWrong     = "Произошла ошибка. Am souwy😔"
 	ErrorStateDataUpdate       = "Произошла ошибка при сохранении данных состояния."
 	ErrorSetState              = "Произошла ошибка при смене состояния."
-	ErrorCheckNameMustBeTxtMsg = "Название обязательно должно быть текстовым сообщением."
+	ErrorNameMustBeTxtMsg      = "Название обязательно должно быть текстовым сообщением."
+	ErrorItemPriceMustBeIntMsg = "Цена должна быть целым числом без пробелов и других знаков (только цифры)."
 )
