e4873d43aa320faced6146fa97b8d54ed708c1f7
Tesorp1X <rakteam2014@gmail.com>
2025-05-18 05:42:28 +0700

    New inline keyboard: ItemOwnershipSelectorInlineKb

--- models/keyboards.go
+++ models/keyboards.go
@@ -26,3 +26,28 @@ func CheckOwnershipSelectorInlineKb(btnTxt1, unique1, data1, btnTxt2, unique2, d
 	)
 	return selector
 }
+
+// ReplyMarkup for selecting how owns an item
+func ItemOwnershipSelectorInlineKb(btnTxt1, unique1, data1, btnTxt2, unique2, data2, btnTxt3, unique3, data3 string) *tele.ReplyMarkup {
+	var (
+		// Universal markup builders.
+		selector = &tele.ReplyMarkup{}
+
+		// Inline buttons.
+		//
+		// Pressing it will cause the client to
+		// send the bot a callback.
+		//
+		// Make sure Unique stays unique as per button kind
+		// since it's required for callback routing to work.
+		//
+		btnLeft   = selector.Data(btnTxt1, unique1, data1)
+		btnRight  = selector.Data(btnTxt2, unique2, data2)
+		btnMiddle = selector.Data(btnTxt3, unique3, data3)
+	)
+	selector.Inline(
+		selector.Row(btnLeft, btnRight),
+		selector.Row(btnMiddle),
+	)
+	return selector
+}
