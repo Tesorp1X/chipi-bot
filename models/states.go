26750271def136f7470a36c5c926c7bacd60ec8f
Tesorp1X <rakteam2014@gmail.com>
2025-05-18 03:50:12 +0700

    package models -- initial commit

---
+++ models/states.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"strings"
+
+	"github.com/vitaliy-ukiru/fsm-telebot/v2"
+)
+
+const (
+	StateDefault           fsm.State = "default"
+	StateStart             fsm.State = "start"
+	StateWaitForCheckName  fsm.State = "wait_for_check_name"
+	StateWaitForCheckOwner fsm.State = "wait_for_check_owner"
+	StateWaitForItemName   fsm.State = "wait_for_item_name"
+	StateWaitForItemPrice  fsm.State = "wait_for_item_price"
+	StateWaitForItemOwner  fsm.State = "wait_for_item_owner"
+	StateWaitForNewItem    fsm.State = "wait_for_new_item"
+)
+
+type CallbackAction string
+
+func (a CallbackAction) String() string {
+	return string(a)
+}
+
+func (a CallbackAction) DataMatches(data string) bool {
+	cringePrefix := "\f" + a.String()
+	return data == cringePrefix || strings.HasPrefix(data, cringePrefix+"|")
+}
+
+const (
+	CallbackActionCheckOwner CallbackAction = "check_owner"
+	CallbackActionItemOwner  CallbackAction = "item_owner"
+	CallbackActionHasNewItem CallbackAction = "has_new_item"
+)
