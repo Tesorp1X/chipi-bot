9230ececd29fec5bb90196998cebb245e51c008e
Tesorp1X <rakteam2014@gmail.com>
2025-05-18 03:54:00 +0700

    chore: doc update

--- util/util.go
+++ util/util.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Tesorp1X/chipi-bot/models"
 )
 
+// Extracts data from [Callback.Data] by removing prefix '\f + CallbackAction + |'
 func ExtractDataFromCallback(data string, action models.CallbackAction) string {
 	return strings.TrimPrefix(data, "\f"+action.String()+"|")
 }
