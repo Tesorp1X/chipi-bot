033ddc93a735bc1df2f366e505f0b5c37aa70a4e
Tesorp1X <rakteam2014@gmail.com>
2025-05-18 04:28:42 +0700

    initial commit

---
+++ models/str_consts.go
@@ -0,0 +1,15 @@
+package models
+
+const (
+	CHECK_OWNER = "check_owner"
+	CHECK_NAME  = "check_name"
+	OWNER_LIZ   = "liz"
+	OWNER_PAU   = "pau"
+)
+
+// error msgs
+const (
+	ErrorSometingWentWrong = "Произошла ошибка. Am souwy😔"
+	ErrorStateDataUpdate   = "Произошла ошибка при сохранении данных состояния."
+	ErrorSetState          = "Произошла ошибка при смене состояния."
+)
