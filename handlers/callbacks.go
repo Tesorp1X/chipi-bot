03de5e09777acabef612f2f1341a4af8917467d9
Tesorp1X <rakteam2014@gmail.com>
2025-06-06 23:01:13 +0700

    New callback handler: ShowTotalsMenuButtonCallback.

--- handlers/callbacks.go
+++ handlers/callbacks.go
@@ -38,6 +38,10 @@ func HandleCallbackAction(c tele.Context, state fsm.Context) error {
 		models.CallbackActionHasNewItem.DataMatches(c.Callback().Data):
 
 		return NewItemCallback(c, state)
+	case currentState == models.StateShowingTotals &&
+		models.CallbackActionMenuButtonPress.DataMatches(c.Callback().Data):
+
+		return ShowTotalsMenuButtonCallback(c, state)
 
 	default:
 		//Response to callback
@@ -261,4 +265,87 @@ func NewItemCallback(c tele.Context, state fsm.Context) error {
 		return c.Send(models.ErrorSetState)
 	}
 	return c.Send(msg)
+}
+
+// Handles buuton-presses('<<' and '>>'), while scrolling through checks in '/show totals'.
+func ShowTotalsMenuButtonCallback(c tele.Context, state fsm.Context) error {
+	// retrieve totals from contexxt or db
+	var totals []*models.SessionTotal
+	if err := state.Data(context.TODO(), models.SESSION_TOTALS, &totals); err != nil {
+		totals, err = db.GetAllSessionTotals()
+		if err != nil {
+			return c.Respond(&tele.CallbackResponse{
+				Text: models.ErrorSometingWentWrong + " Попробуйте еще раз.",
+			})
+		}
+	}
+	// retrieve currentIndex from contexxt or it will be zero
+	var currentIndex int = 0
+	if err := state.Data(context.TODO(), models.CURRENT_INDEX, &currentIndex); err != nil {
+		return c.Respond(&tele.CallbackResponse{
+			Text: models.ErrorSometingWentWrong + " Попробуйте еще раз.",
+		})
+	}
+	if currentIndex < 0 || currentIndex >= len(totals) {
+		currentIndex = 0
+	}
+
+	buttonPressed := util.ExtractDataFromCallback(c.Callback().Data, models.CallbackActionMenuButtonPress)
+	switch buttonPressed {
+	case models.FORWARD:
+		currentIndex++
+		if currentIndex == len(totals) {
+			// to eliminate OutOfBounds Error
+			if err := c.Respond(&tele.CallbackResponse{
+				Text: "Это последний отчет!",
+			}); err != nil {
+				log.Fatalf("couldn't respond to callback %v: %v", c.Callback(), err)
+				return err
+			}
+			return nil
+		}
+	case models.BACK:
+		currentIndex--
+		if currentIndex < 0 {
+			// to eliminate OutOfBounds Error
+
+			if err := c.Respond(&tele.CallbackResponse{
+				Text: "Это первый отчет!",
+			}); err != nil {
+
+				log.Fatalf("couldn't respond to callback %v: %v", c.Callback(), err)
+				return err
+			}
+			return nil
+		}
+	default:
+		return c.Respond(&tele.CallbackResponse{
+			Text: models.ErrorInvalidRequest,
+		})
+	}
+
+	if err := state.Update(context.TODO(), models.CURRENT_INDEX, currentIndex); err != nil {
+		return c.Respond(&tele.CallbackResponse{Text: models.ErrorStateDataUpdate})
+	}
+
+	// if all is good, send empty response. testing kinda...
+	if err := c.Respond(&tele.CallbackResponse{}); err != nil {
+		log.Fatalf("couldn't respond to callback %v: %v", c.Callback(), err)
+	}
+
+	kb := models.CreateSelectorInlineKb(
+		2,
+		models.Button{
+			BtnTxt: "<<",
+			Unique: models.CallbackActionMenuButtonPress.String(),
+			Data:   models.BACK,
+		},
+		models.Button{
+			BtnTxt: ">>",
+			Unique: models.CallbackActionMenuButtonPress.String(),
+			Data:   models.FORWARD,
+		},
+	)
+
+	return c.EditOrReply(util.GetShowTotalsResponse(totals[currentIndex]), kb)
 }
