40233d9c3219c36b258551714a9c470b126c6af9
Tesorp1X <rakteam2014@gmail.com>
2025-06-08 02:13:33 +0700

    New state StateEditingCheck, for edit check feature.

--- models/states.go
+++ models/states.go
@@ -21,6 +21,7 @@ const (
 
 	StateShowingChecks fsm.State = "showing_checks"
 	StateShowingTotals fsm.State = "showing_totals"
+	StateEditingCheck  fsm.State = "editing_check"
 )
 
 type CallbackAction string
