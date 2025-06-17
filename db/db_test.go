afc832c0ada21dbc480984e8feeb09b74a59ca0f
Tesorp1X <rakteam2014@gmail.com>
2025-06-17 13:13:26 +0700

    added init for in-memory db

--- db/db_test.go
+++ db/db_test.go
@@ -1,7 +1,11 @@
 package db
 
 import (
+	"database/sql"
 	"testing"
+
+	"github.com/Tesorp1X/chipi-bot/models"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func TestMakeSqlStmt(t *testing.T) {
@@ -80,3 +84,152 @@ func TestMakeSqlStmt(t *testing.T) {
 		}
 	})
 }
+
+func makeInMemoryDB(t *testing.T) *sql.DB {
+	t.Helper()
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		t.Fatalf("failed to open database: %v", err)
+	}
+
+	sessionFields := []Field{
+		{
+			Name:         "id",
+			Type:         "INTEGER",
+			IsPrimeryKey: true,
+		},
+		{
+			Name: "opened_at",
+			Type: "TEXT", // time as text formated as 2006-01-02 15:04:05
+
+		},
+		{
+			Name:       "closed_at",
+			Type:       "TEXT", // time as text formated as 2006-01-02 15:04:05
+			IsNullable: true,
+		},
+		{
+			Name: "is_open",
+			Type: "TEXT", // bool value as string
+		},
+	}
+	if err = createTable(db, "sessions", sessionFields...); err != nil {
+		t.Fatalf("error couldn't create a db: %v", err)
+	}
+
+	checkFeilds := []Field{
+		{
+			Name:         "id",
+			Type:         "INTEGER",
+			IsPrimeryKey: true,
+		},
+		{
+			Name:       "Name",
+			Type:       "TEXT",
+			IsNullable: false,
+		},
+		{
+			Name:       "Owner",
+			Type:       "TEXT",
+			IsNullable: false,
+		},
+	}
+	if err = createTable(db, "checks", checkFeilds...); err != nil {
+		t.Fatalf("error couldn't create a db: %v", err)
+	}
+
+	itemsFeilds := []Field{
+		{
+			Name:         "id",
+			Type:         "INTEGER",
+			IsPrimeryKey: true,
+		},
+		{
+			Name: "check_id",
+			Type: "INTEGER",
+
+			IsForeignKey: true,
+			RefTableName: "checks",
+			RefFieldName: "id",
+		},
+		{
+			Name: "Name",
+			Type: "TEXT",
+		},
+		{
+			Name: "Owner",
+			Type: "TEXT",
+		},
+		{
+			Name: "Price",
+			Type: "REAL",
+		},
+	}
+	if err = createTable(db, "items", itemsFeilds...); err != nil {
+		t.Fatalf("error couldn't create a db: %v", err)
+
+	}
+
+	totalsField := []Field{
+		{
+			Name:         "id",
+			Type:         "INTEGER",
+			IsPrimeryKey: true,
+		},
+		{
+			Name: "session_id",
+			Type: "INTEGER",
+
+			IsForeignKey: true,
+			RefTableName: "seesions",
+			RefFieldName: "id",
+		},
+		{
+			Name: "total",
+			Type: "REAL",
+		},
+		{
+			Name: "recipient",
+			Type: "TEXT",
+		},
+		{
+			Name: "amount",
+			Type: "REAL",
+		},
+	}
+	if err = createTable(db, "totals", totalsField...); err != nil {
+		t.Fatalf("error couldn't create a db: %v", err)
+
+	}
+
+	checksAndSessionsFields := []Field{
+		{
+			Name:         "id",
+			Type:         "INTEGER",
+			IsPrimeryKey: true,
+		},
+		{
+			Name: "session_id",
+			Type: "INTEGER",
+
+			IsForeignKey: true,
+			RefTableName: "seesions",
+			RefFieldName: "id",
+		},
+		{
+			Name: "check_id",
+			Type: "INTEGER",
+
+			IsForeignKey: true,
+			RefTableName: "checks",
+			RefFieldName: "id",
+		},
+	}
+	if err = createTable(db, "checks_and_sessions", checksAndSessionsFields...); err != nil {
+		t.Fatalf("error couldn't create a db: %v", err)
+
+	}
+
+	return db
+}
+
