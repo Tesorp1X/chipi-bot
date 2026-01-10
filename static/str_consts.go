f934d5cdbcb6aca604a4240073594596ca508d8e
Tesorp1X <artyom.gagin@gmail.com>
2026-01-11 03:30:00 +0700

    string constants from first implementation

---
+++ static/str_consts.go
@@ -0,0 +1,16 @@
+package static
+
+//storage keys
+const (
+	ITEMS_LIST          = "items_list"
+	CURRENT_INDEX_ITEMS = "current_index_items"
+
+	CHECK_ID             = "check_id"
+	CHECK                = "check"
+	CHECKS               = "checks"
+	CURRENT_INDEX_CHECKS = "current_index_checks"
+
+	SESSION_ID           = "session_id"
+	CURRENT_INDEX_TOTALS = "current_index_totals"
+	SESSION_TOTALS       = "session_totals"
+)
