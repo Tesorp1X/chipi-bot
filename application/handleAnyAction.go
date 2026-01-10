b9643cdcb7302dbd72cf799440003123df2060e0
Tesorp1X <artyom.gagin@gmail.com>
2026-01-10 21:45:29 +0700

    added new actions: callback and text

--- application/handleAnyAction.go
+++ application/handleAnyAction.go
@@ -3,6 +3,7 @@ package application
 import (
 	"fmt"
 
+	"github.com/Tesorp1X/chipi-bot/application/callbacks"
 	"github.com/Tesorp1X/chipi-bot/application/handlers"
 
 	"github.com/vitaliy-ukiru/fsm-telebot/v2"
@@ -21,9 +22,17 @@ var allAllowedActionsAndStates []actionsWithStates = []actionsWithStates{
 // default function
 func (app *Application) HandleAnyAction(c tele.Context, state fsm.Context) error {
 	switch {
+	case c.Callback() != nil:
+		if err := callbacks.HandleAnyCallback(app.conf, c, state); err != nil {
+			return fmt.Errorf("error in HandleAnyAction(), action 'OnCallback': %v", err)
+		}
 	case c.Message().Document != nil:
 		if err := handlers.OnDocumentActionHandler(app.conf, c, state); err != nil {
 			return fmt.Errorf("error in HandleAnyAction(), action 'OnDocument': %v", err)
+		}
+	case c.Message().Text != "":
+		if err := handlers.HandleAnyText(app.conf, c, state); err != nil {
+			return fmt.Errorf("error in HandleAnyAction(), action 'OnText': %v", err)
 		}
 	default:
 		return c.Send("unknown action")
