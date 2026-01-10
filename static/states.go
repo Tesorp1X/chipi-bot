065bf61a980a2a94b200738dbd5e42b69d83cf99
Tesorp1X <artyom.gagin@gmail.com>
2026-01-10 21:42:10 +0700

    states from first bot implementation

---
+++ static/states.go
@@ -0,0 +1,25 @@
+package static
+
+import (
+	"github.com/vitaliy-ukiru/fsm-telebot/v2"
+)
+
+// FSM-states
+const (
+	StateDefault fsm.State = "default"
+
+	StateStart fsm.State = "start"
+
+	StateWaitForCheckName    fsm.State = "wait_for_check_name"
+	StateWaitForNewCheckName fsm.State = "wait_for_new_check_name"
+	StateWaitForCheckOwner   fsm.State = "wait_for_check_owner"
+
+	StateWaitForItemName  fsm.State = "wait_for_item_name"
+	StateWaitForItemPrice fsm.State = "wait_for_item_price"
+	StateWaitForItemOwner fsm.State = "wait_for_item_owner"
+	StateWaitForNewItem   fsm.State = "wait_for_new_item"
+
+	StateShowingChecks fsm.State = "showing_checks"
+	StateShowingTotals fsm.State = "showing_totals"
+	StateEditingCheck  fsm.State = "editing_check"
+)
