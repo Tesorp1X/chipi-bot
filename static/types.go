7137d098666b581bfe4ff3c30560f2bd00b948ab
Tesorp1X <artyom.gagin@gmail.com>
2026-01-11 03:29:27 +0700

    implemented Check type with IsEqual method and CreateCheckFromCheckData

--- static/types.go
+++ static/types.go
@@ -1,6 +1,12 @@
 package static
 
-import "strings"
+import (
+	"strings"
+	"time"
+
+	"github.com/Tesorp1X/chipi-bot/utils"
+	"github.com/Tesorp1X/chipi-bot/utils/reader"
+)
 
 type CallbackAction string
 
@@ -17,3 +23,45 @@ const (
 	//CallbackAction<name> CallbackAction = "name"
 	CallbackActionKeep CallbackAction = "keep"
 )
+
+// Represents a record in checks table
+type Check struct {
+	// Record id in table
+	Id int64
+
+	SessionId int64
+
+	// Check name
+	Name string
+	// Shop name
+	OrgName string
+	// Who paid for this check
+	Owner string
+
+	Total    float64
+	TotalPau float64
+	TotalLiz float64
+
+	Date *time.Time
+}
+
+func (a *Check) IsEqual(b *Check) bool {
+	fact := a.Id == b.Id && a.SessionId == b.SessionId
+	fact = fact && a.Name == b.Name && a.OrgName == b.OrgName
+	fact = fact && a.Total == b.Total && a.TotalLiz == b.TotalLiz
+	fact = fact && a.TotalPau == b.TotalPau
+	fact = fact && a.Date.Equal(*b.Date)
+
+	return fact
+}
+
+// Creates an object of `Check`-type from `reader.CheckData` object.
+// Some field are left unfilled.
+func CreateCheckFromCheckData(cd *reader.CheckData) *Check {
+	return &Check{
+		Name:    utils.AssumeCheckName(cd.OrgName),
+		OrgName: cd.OrgName,
+		Total:   cd.Total,
+		Date:    &cd.TimeOfCreation,
+	}
+}
