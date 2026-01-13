a9b6cac3d37464c858e2a65ff3b8c93b21e89f9e
Tesorp1X <artyom.gagin@gmail.com>
2026-01-14 02:59:14 +0700

    implemented handleKeepCheckNameCallback

--- application/callbacks/callbacks.go
+++ application/callbacks/callbacks.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Tesorp1X/chipi-bot/config"
 	"github.com/Tesorp1X/chipi-bot/static"
+	"github.com/Tesorp1X/chipi-bot/utils/responses"
 	"github.com/vitaliy-ukiru/fsm-telebot/v2"
 	tele "gopkg.in/telebot.v4"
 )
@@ -23,9 +24,14 @@ func HandleAnyCallback(conf *config.Config, c tele.Context, state fsm.Context) e
 
 	switch {
 	case currentState == static.StateWaitForCheckName &&
-		static.CallbackActionKeep.DataMatches(callbackData):
+		static.CallbackActionSelector.DataMatches(callbackData):
 		if err := handleKeepCheckNameCallback(conf, c, state); err != nil {
-			return fmt.Errorf("error in HandleAnyCallback(), state 'StateWaitForCheckName', action 'CallbackActionKeep': %v", err)
+			return fmt.Errorf("error in HandleAnyCallback(), state 'StateWaitForCheckName', action 'CallbackActionSelector': %v", err)
+		}
+	case currentState == static.StateShowingAnItem &&
+		static.CallbackActionSelector.DataMatches(callbackData):
+		if err := handleShowingAnItemCallback(conf, c, state); err != nil {
+			return fmt.Errorf("error in HandleAnyCallback(), state 'StateShowingAnItem', action 'CallbackActionSelector': %v", err)
 		}
 	default:
 		// if callback query is old, remove inline buttons from that message
@@ -36,5 +42,90 @@ func HandleAnyCallback(conf *config.Config, c tele.Context, state fsm.Context) e
 }
 
 func handleKeepCheckNameCallback(conf *config.Config, c tele.Context, state fsm.Context) error {
+	// send ok-message
+	// show first item and increment currentIndex
+
+	//get first item and start verification of items
+	var items []*static.Item
+	if err := state.Data(context.Background(), static.ITEMS_LIST, &items); err != nil {
+		sendErr := c.Send("error: couldn't retrieve data from context")
+		return fmt.Errorf(
+			"error in handleCheckName(): couldn't retrieve items from state-storage (%v). send with error: %v",
+			err,
+			sendErr,
+		)
+	}
+
+	if len(items) == 0 {
+		sendErr := c.Send("error: retrieved items-list iss empty")
+		return fmt.Errorf(
+			"error in handleCheckName(): retrieved items-list is empty. send with error: %v",
+			sendErr,
+		)
+	}
+
+	// First message
+	// Replying ok!
+	if sendErr := c.Send("Хорошо. Название не меняем👌"); sendErr != nil {
+		return fmt.Errorf(
+			"error in handleCheckName(): couldn't send an 'ok'-message (%v)",
+			sendErr,
+		)
+	}
+
+	var currentIndex int
+
+	if err := state.SetState(context.Background(), static.StateShowingAnItem); err != nil {
+		sendErr := c.Send("error: couldn't change state")
+		return fmt.Errorf(
+			"error in handleCheckName(): couldn't change a state to StateShowingAnItems (%v). send with error: %v",
+			err,
+			sendErr,
+		)
+	}
+
+	responseTxt, kb := responses.GetItemVerificationResponse(
+		items[currentIndex],
+		currentIndex, len(items),
+	)
+
+	currentIndex++ // now points at the next item
+	if err := state.Update(context.Background(), static.CURRENT_INDEX_ITEMS, currentIndex); err != nil {
+		sendErr := c.Send("error: couldn't save data in context")
+		return fmt.Errorf(
+			"error in handleCheckName(): couldn't save current index in state-storage (%v). send with error: %v",
+			err,
+			sendErr,
+		)
+	}
+
+	// Second message
+	if sendErr := c.Send(responseTxt, kb); sendErr != nil {
+		return fmt.Errorf(
+			"error in handleCheckName(): couldn't send a 'item verification'-message (%v)",
+			sendErr,
+		)
+	}
+
 	return nil
+}
+
+func handleShowingAnItemCallback(conf *config.Config, c tele.Context, state fsm.Context) error {
+	switch c.Callback().Unique {
+	case static.CallbackSelectorChange:
+		// add new line of text at the bottom of that msg "Что меняем?"
+		// change inline kb for that msg to a new one
+		// buttons (two in a row): название, цена, кол-во, сумма
+		// state to waitingForMenuAction maybe
+		return nil
+	case static.CallbackSelectorKeep:
+		// ask who's the owner of an item
+		// new message with a question and inline kb
+		// buttons: liz, both, pau
+		// state to waitingForItemOwner
+		return nil
+	default:
+		return c.Respond(&tele.CallbackResponse{Text: "error todo"})
+	}
+
 }
