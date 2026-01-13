697009a41fec05f9ead0a5a5bbc85538ef94000b
Tesorp1X <artyom.gagin@gmail.com>
2026-01-14 02:53:25 +0700

    implemented handleCheckName

--- application/handlers/handlers.go
+++ application/handlers/handlers.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Tesorp1X/chipi-bot/config"
 	"github.com/Tesorp1X/chipi-bot/static"
+	"github.com/Tesorp1X/chipi-bot/utils/responses"
 	"github.com/vitaliy-ukiru/fsm-telebot/v2"
 	tele "gopkg.in/telebot.v4"
 )
@@ -34,7 +35,87 @@ func HandleAnyText(conf *config.Config, c tele.Context, state fsm.Context) error
 
 func handleCheckName(conf *config.Config, c tele.Context, state fsm.Context) error {
 	//get checkData obj from context
+	var check *static.Check
+	if err := state.Data(context.Background(), static.CHECK, &check); err != nil {
+		sendErr := c.Send("error: couldn't retrieve data from context")
+		return fmt.Errorf(
+			"error in handleCheckName(): couldn't retrieve check from state-storage (%v). send with error: %v",
+			err,
+			sendErr,
+		)
+	}
 	//save new check name in it and put new version in context
+	check.Name = c.Message().Text
+	if err := state.Update(context.Background(), static.CHECK, check); err != nil {
+		sendErr := c.Send("error: couldn't save data in context")
+		return fmt.Errorf(
+			"error in handleCheckName(): couldn't save check in state-storage (%v). send with error: %v",
+			err,
+			sendErr,
+		)
+	}
+
 	//get first item and start verification of items
+	var items []*static.Item
+	if err := state.Data(context.Background(), static.ITEMS_LIST, &items); err != nil {
+		sendErr := c.Send("error: couldn't retrieve data from context")
+		return fmt.Errorf(
+			"error in handleCheckName(): couldn't retrieve items from state-storage (%v). send with error: %v",
+			err,
+			sendErr,
+		)
+	}
+
+	if len(items) == 0 {
+		sendErr := c.Send("error: retrieved items-list iss empty")
+		return fmt.Errorf(
+			"error in handleCheckName(): retrieved items-list is empty. send with error: %v",
+			sendErr,
+		)
+	}
+
+	// First message
+	// Replying ok!
+	if sendErr := c.Send("Отлично, название изменено на <b>" + check.Name + "</b>!"); sendErr != nil {
+		return fmt.Errorf(
+			"error in handleCheckName(): couldn't send an 'ok'-message (%v)",
+			sendErr,
+		)
+	}
+
+	var currentIndex int
+
+	if err := state.SetState(context.Background(), static.StateShowingAnItem); err != nil {
+		sendErr := c.Send("error: couldn't change state")
+		return fmt.Errorf(
+			"error in handleCheckName(): couldn't change a state to StateShowingAnItems (%v). send with error: %v",
+			err,
+			sendErr,
+		)
+	}
+
+	responseTxt, kb := responses.GetItemVerificationResponse(
+		items[currentIndex],
+		currentIndex, len(items),
+	)
+
+	currentIndex++ // now points at the next item
+	if err := state.Update(context.Background(), static.CURRENT_INDEX_ITEMS, currentIndex); err != nil {
+		sendErr := c.Send("error: couldn't save data in context")
+		return fmt.Errorf(
+			"error in handleCheckName(): couldn't save current index in state-storage (%v). send with error: %v",
+			err,
+			sendErr,
+		)
+	}
+
+	// Second message
+	if sendErr := c.Send(responseTxt, kb); sendErr != nil {
+		return fmt.Errorf(
+			"error in handleCheckName(): couldn't send a 'item verification'-message (%v)",
+			sendErr,
+		)
+	}
+
 	return nil
 }
