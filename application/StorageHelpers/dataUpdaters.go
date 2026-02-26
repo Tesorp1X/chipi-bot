40463bb4f1eb4235ae7f55434a395a8195bb772d
Tesorp1X <artyom.gagin@gmail.com>
2026-02-26 10:10:28 +0700

    new function UpdateItemsList

--- application/StorageHelpers/dataUpdaters.go
+++ application/StorageHelpers/dataUpdaters.go
@@ -16,7 +16,7 @@ func UpdateCurrentItemsIndex(newCurrentIndex int, c tele.Context, state fsm.Cont
 	if err := state.Update(context.Background(), static.CURRENT_INDEX_ITEMS, newCurrentIndex); err != nil {
 		sendErr := c.Send("error: couldn't save data in context")
 		return fmt.Errorf(
-			"error in UpdateCurrentIndex(): couldn't save current index in state-storage (%v). send with error: %v",
+			"error in UpdateCurrentIndex(): couldn't save current index in state-storage (%v). sent with error: %v",
 			err,
 			sendErr,
 		)
@@ -31,7 +31,22 @@ func UpdateCheck(check *static.Check, c tele.Context, state fsm.Context) error {
 	if err := state.Update(context.Background(), static.CHECK, check); err != nil {
 		sendErr := c.Send("error: couldn't save data in context")
 		return fmt.Errorf(
-			"error in UpdateCheck(): couldn't save check in state-storage (%v). send with error: %v",
+			"error in UpdateCheck(): couldn't save check in state-storage (%v). sent with error: %v",
+			err,
+			sendErr,
+		)
+	}
+
+	return nil
+}
+
+// Updates a ITEMS_LIST value in a context-storage.
+// If error occurs, it will bbe wrapped and returned.
+func UpdateItemsList(items []*static.Item, c tele.Context, state fsm.Context) error {
+	if err := state.Update(context.Background(), static.ITEMS_LIST, items); err != nil {
+		sendErr := c.Send("error: couldn't save data in context")
+		return fmt.Errorf(
+			"error in UpdateItemsList(): couldn't save items in state-storage (%v). sent with error: %v",
 			err,
 			sendErr,
 		)
