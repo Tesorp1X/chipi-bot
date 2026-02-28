1a6c68682080da5f74a88d6c00507acde9dcbbc9
Tesorp1X <artyom.gagin@gmail.com>
2026-02-28 07:48:16 +0700

    added a createTable method

--- db/db.go
+++ db/db.go
@@ -27,3 +27,35 @@ func MakeNewDBService(dsnURI string) (*DBService, error) {
 
 	return &DBService{db: db}, nil
 }
+
+// Creates a table with provided name and fields.
+// If error occurs, returns a wrapped error.
+func (dbs *DBService) createTable(name string, fields ...*field) error {
+	sqlStmt, err := makeSqlStmtCreateTable(name, fields...)
+
+	if err != nil {
+		return fmt.Errorf(
+			"error in db.createTable(): couldn't prepare a sql-statement (%v)",
+			err,
+		)
+	}
+
+	createTableStatement, err := dbs.db.Prepare(sqlStmt)
+	if err != nil {
+
+		return fmt.Errorf(
+			"error in db.createTable(): couldn't prepare a db query (%v)",
+			err,
+		)
+	}
+
+	_, err = createTableStatement.Exec()
+	if err != nil {
+		return fmt.Errorf(
+			"error in db.createTable(): couldn't execute a statement (%v)",
+			err,
+		)
+	}
+
+	return nil
+}
