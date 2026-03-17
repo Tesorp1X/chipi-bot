69018f82f6d8c8fe0b323e4fe3d3069533737020
Tesorp1X <artyom.gagin@gmail.com>
2026-03-18 03:28:25 +0700

    added consts for every column name

--- db/tables.go
+++ db/tables.go
@@ -14,124 +14,181 @@ const (
 	SESSIONS_TABLE_NAME = "sessions"
 )
 
+// Column names
+const (
+	// Checks
+
+	CHECKS_ID               = "id"
+	CHECKS_SESSION_ID       = "session_id"
+	CHECKS_NAME             = "name"
+	CHECKS_ORGNAME          = "orgname"
+	CHECKS_OWNER            = "owner"
+	CHECKS_TOTAL            = "total"
+	CHECKS_TOTAL_PAU        = "total_pau"
+	CHECKS_TOTAL_LIZ        = "total_liz"
+	CHECKS_DATE_OF_PURCHASE = "date_of_purchase"
+
+	// Items
+
+	ITEMS_ID       = "id"
+	ITEMS_CHECK_ID = "check_id"
+	ITEMS_NAME     = "name"
+	ITEMS_OWNER    = "owner"
+	ITEMS_PRICE    = "price"
+	ITEMS_AMOUNT   = "amount"
+	ITEMS_SUBTOTAL = "subtotal"
+
+	// Sessions
+
+	//SESSIONS_
+	SESSIONS_ID              = "id"
+	SESSIONS_OPENED_AT       = "opened_at"
+	SESSIONS_CLOSED_AT       = "closed_at"
+	SESSIONS_IS_OPEN         = "is_open"
+	SESSIONS_TOTAL_SPENT     = "total_spent"
+	SESSIONS_PAU_TOTAL_SPENT = "total_pau_spent"
+	SESSIONS_LIZ_TOTAL_SPENT = "total_liz_spent"
+	SESSIONS_TOPUPS          = "total_topups"
+	SESSIONS_PAU_TOPUPS      = "total_pau_topups"
+	SESSIONS_LIZ_TOPUPS      = "total_liz_topups"
+)
+
 var (
 	checksTableFields []*field = []*field{
 		{
-			Name:         "id",
+			Name:         CHECKS_ID,
 			Type:         IntegerType,
 			IsPrimaryKey: true,
 		},
 		{
-			Name: "session_id",
+			Name: CHECKS_SESSION_ID,
 			Type: IntegerType,
 
 			IsForeignKey: true,
 			RefTableName: SESSIONS_TABLE_NAME,
-			RefFieldName: "id",
+			RefFieldName: SESSIONS_ID,
 		},
 		{
-			Name: "name",
+			Name: CHECKS_NAME,
 			Type: TextType,
 		},
 		{
-			Name: "orgname",
+			Name: CHECKS_ORGNAME,
 			Type: TextType,
 		},
 		{
-			Name: "owner",
+			Name: CHECKS_OWNER,
 			Type: TextType,
 		},
 		{
-			Name: "total",
+			Name: CHECKS_TOTAL,
 			Type: RealType,
 		},
 		{
-			Name: "total_pau",
+			Name: CHECKS_TOTAL_PAU,
 			Type: RealType,
 		},
 		{
-			Name: "total_liz",
+			Name: CHECKS_TOTAL_LIZ,
 			Type: RealType,
 		},
 		{
-			Name: "date_of_purchase",
+			Name: CHECKS_DATE_OF_PURCHASE,
 			Type: TextType,
 		},
 	}
 
 	itemsTableFields []*field = []*field{
 		{
-			Name:         "id",
+			Name:         ITEMS_ID,
 			Type:         IntegerType,
 			IsPrimaryKey: true,
 		},
 		{
-			Name: "check_id",
+			Name: ITEMS_CHECK_ID,
 			Type: IntegerType,
 
 			IsForeignKey: true,
 			RefTableName: CHECKS_TABLE_NAME,
-			RefFieldName: "id",
+			RefFieldName: CHECKS_ID,
 		},
 		{
-			Name: "name",
+			Name: ITEMS_NAME,
 			Type: TextType,
 		},
 		{
-			Name: "owner",
+			Name: ITEMS_OWNER,
 			Type: TextType,
 		},
 		{
-			Name: "price",
+			Name: ITEMS_PRICE,
 			Type: RealType,
 		},
 		{
-			Name: "amount",
+			Name: ITEMS_AMOUNT,
 			Type: RealType,
 		},
 		{
-			Name: "subtotal",
+			Name: ITEMS_SUBTOTAL,
 			Type: RealType,
 		},
 	}
 
 	sessionsTableFields []*field = []*field{
 		{
-			Name:         "id",
+			Name:         SESSIONS_ID,
 			Type:         IntegerType,
 			IsPrimaryKey: true,
 		},
 		{
-			Name: "opened_at",
+			Name: SESSIONS_OPENED_AT,
 			Type: TextType,
 		},
 		{
-			Name: "closed_at",
+			Name: SESSIONS_CLOSED_AT,
+			Type: TextType,
+
+			IsNullable: true,
+		},
+		{
+			Name: SESSIONS_IS_OPEN,
 			Type: TextType,
 		},
 		{
-			Name: "total_spent",
+			Name: SESSIONS_TOTAL_SPENT,
 			Type: RealType,
+
+			IsNullable: true,
 		},
 		{
-			Name: "total_pau_spent",
+			Name: SESSIONS_PAU_TOTAL_SPENT,
 			Type: RealType,
+
+			IsNullable: true,
 		},
 		{
-			Name: "total_liz_spent",
+			Name: SESSIONS_LIZ_TOTAL_SPENT,
 			Type: RealType,
+
+			IsNullable: true,
 		},
 		{
-			Name: "total_topups",
+			Name: SESSIONS_TOPUPS,
 			Type: RealType,
+
+			IsNullable: true,
 		},
 		{
-			Name: "total_pau_topups",
+			Name: SESSIONS_PAU_TOPUPS,
 			Type: RealType,
+
+			IsNullable: true,
 		},
 		{
-			Name: "total_liz_topups",
+			Name: SESSIONS_LIZ_TOPUPS,
 			Type: RealType,
+
+			IsNullable: true,
 		},
 	}
 )
